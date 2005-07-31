264687c1c341de62eea384ec8efe54f885afd108
carlosgc <carlosgc>
2005-07-31 17:25:13 +0000

    2005-07-30  Carlos Garcia Campos  <carlosgc@gnome.org> 	* src/Makefile.am: 	* src/buoh-comic-loader.[ch]: Added 	* src/buoh-view.[ch], src/buoh-window.[ch]: Remove view status. 	We only need to know the current page of the view, the comic load 	status it's now in the BuohComicLoader object. 	Use a thread for the comic loading stuff.

---
+++ src/buoh-comic-loader.h
@@ -0,0 +1,75 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  Authors : Carlos García Campos <carlosgc@gnome.org>
+ */
+
+#ifndef BUOH_COMIC_LOADER_H
+#define BUOH_COMIC_LOADER_H
+
+#include <glib-object.h>
+#include <glib.h>
+#include <gdk-pixbuf/gdk-pixbuf.h>
+
+#include "buoh-comic.h"
+
+G_BEGIN_DECLS
+
+enum {
+	LOADER_STATE_READY,
+	LOADER_STATE_RUNNING,
+	LOADER_STATE_FINISHED,
+	LOADER_STATE_STOPPING,
+	LOADER_STATE_FAILED
+};
+
+typedef struct _BuohComicLoader        BuohComicLoader;
+typedef struct _BuohComicLoaderClass   BuohComicLoaderClass;
+
+#define BUOH_TYPE_COMIC_LOADER                  (buoh_comic_loader_get_type())
+#define BUOH_COMIC_LOADER(object)               (G_TYPE_CHECK_INSTANCE_CAST((object), BUOH_TYPE_COMIC_LOADER, BuohComicLoader))
+#define BUOH_COMIC_LOADER_CLASS(klass)          (G_TYPE_CHACK_CLASS_CAST((klass), BUOH_TYPE_COMIC_LOADER, BuohComicLoaderClass))
+#define BUOH_IS_COMIC_LOADER(object)            (G_TYPE_CHECK_INSTANCE_TYPE((object), BUOH_TYPE_COMIC_LOADER))
+#define BUOH_IS_COMIC_LOADER_CLASS(klass)       (G_TYPE_CHECK_CLASS_TYPE((klass), BUOH_TYPE_COMIC_LOADER))
+#define BUOH_COMIC_LOADER_GET_CLASS(object)     (G_TYPE_INSTANCE_GET_CLASS((object), BUOH_TYPE_COMIC_LOADER, BuohComicLoaderClass))
+
+struct _BuohComicLoader {
+	GObject    parent;
+
+	gchar     *uri;
+	
+	GMutex    *thread_mutex;
+	GMutex    *pixbuf_mutex;
+	GMutex    *status_mutex;
+	
+	GThread   *thread;
+	GdkPixbuf *pixbuf;
+	guint      status;
+};
+
+struct _BuohComicLoaderClass {
+	GObjectClass   parent_class;
+};
+
+GType            buoh_comic_loader_get_type (void);
+BuohComicLoader *buoh_comic_loader_new      (void);
+
+void             buoh_comic_loader_run      (BuohComicLoader *loader,
+					     const gchar     *uri);
+void             buoh_comic_loader_stop     (BuohComicLoader *loader);
+
+G_END_DECLS
+
+#endif /* !BUOH_COMIC_LOADER_H */
