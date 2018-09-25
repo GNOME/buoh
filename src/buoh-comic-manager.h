bbdf03e6cff08bbc4709f23a87ede0fe36e90646
Jan Tojnar <jtojnar@gmail.com>
2018-09-25 02:07:07 +0200

    Rewrite comic-manager to use G_DECLARE_DERIVABLE_TYPE

--- src/buoh-comic-manager.h
+++ src/buoh-comic-manager.h
@@ -24,23 +24,8 @@
 
 G_BEGIN_DECLS
 
-typedef struct _BuohComicManager        BuohComicManager;
-typedef struct _BuohComicManagerClass   BuohComicManagerClass;
-typedef struct _BuohComicManagerPrivate BuohComicManagerPrivate;
-
-#define BUOH_TYPE_COMIC_MANAGER                (buoh_comic_manager_get_type ())
-#define BUOH_COMIC_MANAGER(o)                (G_TYPE_CHECK_INSTANCE_CAST ((o), BUOH_TYPE_COMIC_MANAGER, BuohComicManager))
-#define BUOH_COMIC_MANAGER_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), BUOH_TYPE_COMIC_MANAGER, BuohComicManagerClass))
-#define BUOH_IS_COMIC_MANAGER(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), BUOH_TYPE_COMIC_MANAGER))
-#define BUOH_IS_COMIC_MANAGER_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), BUOH_TYPE_COMIC_MANAGER))
-#define BUOH_COMIC_MANAGER_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), BUOH_TYPE_COMIC_MANAGER, BuohComicManagerClass))
-
-
-struct _BuohComicManager {
-        GObject parent;
-
-        BuohComicManagerPrivate *priv;
-};
+#define BUOH_TYPE_COMIC_MANAGER buoh_comic_manager_get_type ()
+G_DECLARE_DERIVABLE_TYPE (BuohComicManager, buoh_comic_manager, BUOH, COMIC_MANAGER, GObject)
 
 struct _BuohComicManagerClass {
         GObjectClass      parent_class;
